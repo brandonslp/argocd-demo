f62a1959a2385e8633da58dc95b5fcfa29134b4c
brandonslp <blopez@formax.cl>
2022-06-23 13:10:23 -0500

    replace and show

---
+++ updater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SEARCH=$1
+NEW_VERSION=$2
+CURRENT_VERSION=$(cat ./example.yaml  | grep -A3 ${SEARCH} | grep tag: | awk '{ print $(NF - 0); }')
+echo "Replace $CURRENT_VERSION by $NEW_VERSION "
+TEXT="container:
+      image:
+        tag:"
+  
+perl -i -0pe "s/${SEARCH}:
+    ${TEXT} ${CURRENT_VERSION}/${SEARCH}:
+    ${TEXT} ${NEW_VERSION}/" ./example.yaml
